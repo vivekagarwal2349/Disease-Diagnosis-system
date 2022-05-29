d148cb55260aa78650bb1366f07f1afa77f0d4c1
vivek <vivek.agarwal.ece20@itbhu.ac.in>
2022-05-29 15:21:02 +0530

    comment

--- face_detect/views.py
+++ face_detect/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.models import User
 from .models import *
-import cv2,time
+import cv2
 import numpy as np
 from django.http import JsonResponse
 from os import listdir
